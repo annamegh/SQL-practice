4584a5f51bd23d99d6210579b696aba94c03609e
Anna Paula Meneghelli de Oliveira <130464385+annamegh@users.noreply.github.com>
2024-08-20 20:18:31 -0300

    Update and rename Medium: Questões 21-25 to medium_questoes_21_25.sql

--- medium_questoes_21_25.sql
+++ medium_questoes_21_25.sql
@@ -1,4 +1,4 @@
-21. Display the total amount of patients for each province. Order by descending.
+--21. Display the total amount of patients for each province. Order by descending.
 
   SELECT
     province_name,
@@ -8,7 +8,7 @@
   GROUP BY province_name
   ORDER BY count(*) DESC;
 
-22. For every admission, display the patient's full name, their admission diagnosis, and their doctor's full name who diagnosed their problem.
+--22. For every admission, display the patient's full name, their admission diagnosis, and their doctor's full name who diagnosed their problem.
 
   SELECT
     p.first_name || ' ' || p.last_name as full_name,
@@ -18,7 +18,7 @@
     JOIN patients p ON p.patient_id = a.patient_id
     JOIN doctors d ON d.doctor_id = a.attending_doctor_id
 
-23. Display the number of duplicate patients based on their first_name and last_name.
+--23. Display the number of duplicate patients based on their first_name and last_name.
 
   SELECT
     first_name, last_name,
@@ -27,10 +27,10 @@
   group by first_name, last_name
   having count(*) = 2;
 
-24. Display patient's full name,
+/*24. Display patient's full name,
 height in the units feet rounded to 1 decimal, weight in the unit pounds rounded to 0 decimals, birth_date, gender non abbreviated.
 Convert CM to feet by dividing by 30.48.
-Convert KG to pounds by multiplying by 2.205.
+Convert KG to pounds by multiplying by 2.205.*/
   
   SELECT
     first_name || ' ' || last_name AS full_name,
@@ -43,8 +43,8 @@ Convert KG to pounds by multiplying by 2.205.
     END AS 'gender'
   FROM patients;
 
-25. Show patient_id, first_name, last_name from patients whose does not have any records in the admissions table. 
-(Their patient_id does not exist in any admissions.patient_id rows.)
+/*25. Show patient_id, first_name, last_name from patients whose does not have any records in the admissions table. 
+(Their patient_id does not exist in any admissions.patient_id rows.)*/
 
   SELECT
     patient_id,
