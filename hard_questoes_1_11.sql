d8b87990c1f19e471382d4138ea9f55d4be38b21
Anna Paula Meneghelli de Oliveira <130464385+annamegh@users.noreply.github.com>
2024-08-20 20:15:18 -0300

    Update and rename Hard: Questões 1-11 to hard_questoes_1_11.sql

--- hard_questoes_1_11.sql
+++ hard_questoes_1_11.sql
@@ -1,7 +1,7 @@
-1. Show all of the patients grouped into weight groups.
+/*1. Show all of the patients grouped into weight groups.
 Show the total amount of patients in each weight group.
 Order the list by the weight group decending.
-For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
+For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.*/
 
   SELECT
     count(*) AS patients_in_group,
@@ -10,8 +10,8 @@ For example, if they weight 100 to 109 they are placed in the 100 weight group, 
   GROUP BY weight_group 
   ORDER BY weight_group DESC;
 
-2. Show patient_id, weight, height, isObese from the patients table.
-Display isObese as a boolean 0 or 1. Obese is defined as weight(kg)/(height(m)2) >= 30.
+/*2. Show patient_id, weight, height, isObese from the patients table.
+Display isObese as a boolean 0 or 1. Obese is defined as weight(kg)/(height(m)2) >= 30.*/
 Weight is in units kg. Height is in units cm.
   
   SELECT
@@ -24,9 +24,9 @@ Weight is in units kg. Height is in units cm.
     END AS isObese
   FROM patients;
 
-3. Show patient_id, first_name, last_name, and attending doctor's specialty.
+/*3. Show patient_id, first_name, last_name, and attending doctor's specialty.
 Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
-Check patients, admissions, and doctors tables for required information.
+Check patients, admissions, and doctors tables for required information.*/
 
   SELECT
     p.patient_id,
@@ -40,12 +40,12 @@ Check patients, admissions, and doctors tables for required information.
     a.diagnosis = 'Epilepsy'
     AND d.first_name = 'Lisa';
 
-4. All patients who have gone through admissions, can see their medical documents on our site. 
+/*4. All patients who have gone through admissions, can see their medical documents on our site. 
 Those patients are given a temporary password after their first admission. Show the patient_id and temp_password.
 The password must be the following, in order:
 1. patient_id
 2. the numerical length of patient's last_name
-3. year of patient's birth_date
+3. year of patient's birth_date*/
 
   SELECT
     p.patient_id,
@@ -59,10 +59,10 @@ The password must be the following, in order:
   WHERE a.patient_id IS NOT NULL
   GROUP BY p.patient_id;
 
-5. Each admission costs $50 for patients without i nsurance, and $10 for patients with insurance. 
+/*5. Each admission costs $50 for patients without i nsurance, and $10 for patients with insurance. 
 All patients with an even patient_id have insurance.
 Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. 
-Add up the admission_total cost for each has_insurance group.
+Add up the admission_total cost for each has_insurance group.*/
 
   SELECT
     CASE
@@ -76,7 +76,7 @@ Add up the admission_total cost for each has_insurance group.
   FROM admissions
   group by has_insurance;
 
-6. Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name.
+--6. Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name.
 
   SELECT province_name
   FROM province_names
@@ -102,13 +102,13 @@ OU
   HAVING
     SUM(gender = 'M') > SUM(gender = 'F');
 
-7. We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
+/*7. We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
 - First_name contains an 'r' after the first two letters.
 - Identifies their gender as 'F'
 - Born in February, May, or December
 - Their weight would be between 60kg and 80kg
 - Their patient_id is an odd number
-- They are from the city 'Kingston'
+- They are from the city 'Kingston'*/
 
   SELECT *
   FROM patients
@@ -120,7 +120,7 @@ OU
     AND patient_id%2 != 0
     AND city = 'Kingston';
 
-8. Show the percent of patients that have 'M' as their gender. Round the answer to the nearest hundreth number and in percent form.
+--8. Show the percent of patients that have 'M' as their gender. Round the answer to the nearest hundreth number and in percent form.
 
   SELECT
     DISTINCT CONCAT (
@@ -128,14 +128,14 @@ OU
       '%')
   FROM patients;
 
-OU
+--OU
 
   SELECT
     ROUND(100 * AVG(gender = 'M'), 2) || '%' 
   FROM
     patients;
 
-9. For each day display the total amount of admissions on that day. Display the amount changed from the previous date.
+--9. For each day display the total amount of admissions on that day. Display the amount changed from the previous date.
 
   SELECT
     admission_date,
@@ -145,7 +145,7 @@ OU
   GROUP BY admission_date
   order by admission_date;
 
-10. Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.
+--10. Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.
 
   SELECT distinct province_name
   FROM province_names
@@ -155,8 +155,8 @@ OU
         ELSE 1
       END );
 
-11. We need a breakdown for the total amount of admissions each doctor has started each year. 
-Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.
+/*11. We need a breakdown for the total amount of admissions each doctor has started each year. 
+Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.*/
 
   SELECT
     doctor_id,
